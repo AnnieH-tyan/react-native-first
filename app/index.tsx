2e2eef23825ac13c205f36451a41cd284db20f10
aha <a.halatyan@fixar.pro>
2025-06-17 18:38:07 +0300

    expo router basic

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import "../global.css";
 
@@ -5,6 +6,7 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/login">Login</Link>
     </View>
   );
 }
