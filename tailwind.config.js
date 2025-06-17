bcd92b969fba3ca12571d2941274ef6573fd3567
aha <a.halatyan@fixar.pro>
2025-06-17 18:00:35 +0300

    nativewind setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // style everything within app folders that ends with this extensions
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
